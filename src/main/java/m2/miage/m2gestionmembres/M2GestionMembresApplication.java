76485ddf951bb199b361798ffe216ef502c1ca67
DucTrongVo <duc-trong.vo@toulouse.miage.fr>
2021-03-09 15:26:56 +0100

    first commit

---
+++ src/main/java/m2/miage/m2gestionmembres/M2GestionMembresApplication.java
@@ -0,0 +1,13 @@
+package m2.miage.m2gestionmembres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2GestionMembresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2GestionMembresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/m2/miage/m2gestionmembres/M2GestionMembresApplicationTests.java
@@ -0,0 +1,13 @@
+package m2.miage.m2gestionmembres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2GestionMembresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
