56f0c8e58d133b59a843215dd459a4e5a0584415
DucTrongVo <duc-trong.vo@toulouse.miage.fr>
2021-06-07 13:48:25 +0200

    transactional

--- src/main/java/m2/miage/m2gestionmembres/services/MembreService.java
+++ src/main/java/m2/miage/m2gestionmembres/services/MembreService.java
@@ -3,6 +3,7 @@ package m2.miage.m2gestionmembres.services;
 import javassist.NotFoundException;
 import m2.miage.m2gestionmembres.Exception.ForbiddenException;
 import m2.miage.m2gestionmembres.entities.Membre;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,7 +12,9 @@ public interface MembreService {
     Membre creerMembre(Membre membre) throws NotFoundException, ForbiddenException;
     Membre getMembreByEmail(String email) throws NotFoundException;
     List<Membre> getAllMembre(String emailRequester) throws NotFoundException, ForbiddenException;
+    @Transactional
     Membre mettreAJourMembre(String emailRequester, String email, Membre membreAJour) throws NotFoundException, ForbiddenException;
+    @Transactional
     Boolean supprimerMembre(String emailRequester, String emailMembre) throws NotFoundException, ForbiddenException;
     Boolean isMembreApte(String emailMembre) throws NotFoundException;
 }
